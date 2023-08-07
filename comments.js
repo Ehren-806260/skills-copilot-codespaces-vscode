7c9f369d43eb413f4bd643b60829cfd5c8757dae
技術_大網_陳依仁Ehren <91528957+Ehren-806260@users.noreply.github.com>
2023-08-07 09:10:21 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,26 @@
+// create web server
+// 1. import express
+const express = require('express');
+// 2. create web server object
+const app = express();
+// 3. create router object
+const router = express.Router();
+// 4. register router object
+app.use(router);
+// 5. register router object
+router.get('/comments', (req, res) => {
+    res.send('get comments');
+});
+router.post('/comments', (req, res) => {
+    res.send('post comments');
+});
+router.put('/comments', (req, res) => {
+    res.send('put comments');
+});
+router.delete('/comments', (req, res) => {
+    res.send('delete comments');
+});
+// 6. start web server
+app.listen(3000, () => {
+    console.log('web server is listening port 3000...');
+});
